bdb4755df0930dd1a3dbcc9a3fb4eb066e79ba06
Roy <Roy@Roy-VAIO>
2015-02-17 20:32:12 -0800

    Added Admin controller
    Signed-off-by:Roy <Roy@Roy-VAIO>

---
+++ application/controllers/Admin.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Display one or all of the quotes on file.
+ * 
+ * controllers/Viewer.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class Viewer extends Application {
+
+    function __construct()
+    {
+	parent::__construct();
+    }
+}
