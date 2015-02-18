76efedf360cd81d8c55b411e55182c9184f23319
Roy <Roy@Roy-VAIO>
2015-02-18 14:47:13 -0800

    Added add and present functions to Admin Signed-off-by:Roy <Roy@Roy-VAIO>

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -24,4 +24,20 @@ class Admin extends Application {
         $this->data['pagebody'] = 'admin_list'; 
         $this->render();
     }
+    
+    // Add a new quotation
+    function add() {
+        $quote = $this->quotes->create();
+        $this->present($quote);
+    }
+    
+    // Present a quotation for adding/editing
+    function present($quote) {
+        $this->data['fid'] = makeTextField('ID#', 'id', $quote->id);
+        $this->data['fwho'] = makeTextField('Author', 'who', $quote->who);
+        $this->data['fmug'] = makeTextField('Picture', 'mug', $quote->mug);
+        $this->data['fwhat'] = makeTextArea('The Quote', 'what', $quote->what);
+        $this->data['pagebody'] = 'quote_edit';
+        $this->render();
+    }
 }
