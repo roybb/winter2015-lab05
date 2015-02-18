ea757648b22821a638b43fc51ff63410a3e3989d
Roy <Roy@Roy-VAIO>
2015-02-17 20:36:47 -0800

    Added index function to Admin Signed-off-by:Roy <Roy@Roy-VAIO>

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -7,10 +7,19 @@
  *
  * ------------------------------------------------------------------------
  */
-class Viewer extends Application {
+class Admin extends Application {
 
     function __construct()
     {
 	parent::__construct();
+    }
+    
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+    
+    function index() {
+        $this->data['pagebody'] = 'homepage';
+        $this->render();
     }
 }
